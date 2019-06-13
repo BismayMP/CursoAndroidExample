4fbe5be71ebd9532a0902a3976b48cb8fef7d9f4
bismaymp <bismaymp@gmail.com>
2019-06-13 02:03:57 -0400

    Empezando a manejar datos

--- app/src/main/java/com/xookware/cursoandroidexample/MainActivity.java
+++ app/src/main/java/com/xookware/cursoandroidexample/MainActivity.java
@@ -1,8 +1,13 @@
 package com.xookware.cursoandroidexample;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,15 +18,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.xookware.cursoandroidexample.activities.AsignaturasActivity;
+import com.xookware.cursoandroidexample.database.DataBaseHandler;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DataBaseHandler db;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        db = new DataBaseHandler(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +61,25 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(false);
+            builder.setIcon(getResources().getDrawable(R.drawable.alert));
+            builder.setNegativeButton(getResources().getString(R.string.no), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.setPositiveButton(getResources().getString(R.string.si), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finishAffinity();
+                }
+            });
+            AlertDialog d = builder.create();
+            d.setMessage(getResources().getString(R.string.exit));
+            d.show();
+
         }
     }
 
@@ -79,12 +110,18 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        String toolbar_text = "";
+
+        Intent intent = null;
+        Fragment fragment = null;
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_asignaturas) {
+            intent = new Intent(MainActivity.this, AsignaturasActivity.class);
 
         } else if (id == R.id.nav_manage) {
 
@@ -93,7 +130,16 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
+        if (intent != null) {
+            startActivity(intent);
 
+        } else if (fragment != null) {
+            transaction.replace(R.id.inicio_container, fragment);
+            toolbar.setTitle(toolbar_text);
+            transaction.commit();
+        }
+
+        toolbar.setTitle(toolbar_text);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
